ff52ba431bfe1dbcea04478e7d2f68cf997c2196
Eshan Mitra <emitra@lanl.gov>
2018-03-28 09:05:38 -0600

    Configure for example0 to run on Darwin

--- examples_clean/example0/ex0_batch.sh
+++ examples_clean/example0/ex0_batch.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=4
 
 # Force to run on only nodes 300-371, which are all identical
-#SBATCH --exclude=cn[0-299,372-9999]
+#SBATCH --constraint="nvme:no,baseboard_vendor:HP,cpu_vendor:Intel,cpu_family:broadwell,cpu_model:E5-2695_v4,cpu_base_clock:2.10GHz,numa_nodes:2,clusterondie:no,multithreading:yes,ib:edr,ethernet:1Gb,ssd:no,hdd:no,gpu_count:0"
 
 
 # Require that we get allocated the whole nodes, so we get comparable runs. 
